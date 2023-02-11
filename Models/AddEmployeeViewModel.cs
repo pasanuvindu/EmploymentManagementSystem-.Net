c582c7ff2c3677a18952be7d7eb98c4b7fcbbb9f
Pasan Wadasinghe <69391694+pasanuvindu@users.noreply.github.com>
2023-02-11 16:06:23 +0530

    added all files

---
+++ Models/AddEmployeeViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace WebAppCm.Models
+{
+    public class AddEmployeeViewModel
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public long Salary { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Dipartqment { get; set; }
+    }
+}
